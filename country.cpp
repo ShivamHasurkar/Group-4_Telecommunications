807907cb70cfa5343b9042af9a2b020bfce71f10
Pooja Patel <patelpooja126018@gmail.com>
2021-05-06 22:12:29 +0530

    Adding country.cpp

---
+++ country.cpp
@@ -0,0 +1,93 @@
+#include "country.h"
+#include <map>
+#include <string>
+#include <iostream>
+//#include "ContactDetails..cpp"
+using namespace country;
+using namespace std;
+
+//Default constructor
+Country::Country()
+{
+    this->country = {};
+}
+
+//parameterized constructor
+Country::Country(map<int, string> country, ContactDetails contactdetails)
+{
+    this->country = country;
+    this->contactdetails = contactdetails;
+}
+
+//setting country
+void Country::setCountry(int code, string name)
+{
+    this->country.insert({code, name});
+}
+
+//getter for countryname
+string Country::getCountryName(int code)
+{
+    auto itr = this->country.find(code);
+    return (*itr).second;
+}
+
+//getting all countries
+void Country::printAllCountries()
+{
+    for (auto itr : this->country)
+    {
+        cout << itr.first << " : " << itr.second;
+    }
+}
+
+//count of all countries
+int Country::getCountryCount()
+{
+    return this->country.size();
+}
+
+//updating country
+bool Country::updateCountry(int oldcode, int newcode, string name)
+{
+    try
+    {
+
+        auto itr = this->country.find(oldcode);
+        itr->first = newcode;
+        itr->second = name;
+    }
+    catch (exception exception)
+    {
+        return false;
+    }
+    return true;
+}
+
+//deleting country
+bool Country ::deleteCountry(int code)
+
+{
+    try
+    {
+        int result = this->country.erase(code);
+        if (result)
+            return true;
+    }
+    catch (exception exception)
+    {
+        return false;
+    }
+    return false;
+}
+
+//setting contactdetails
+void Country ::setContactDetails(ContactDetails contactdetails)
+{
+    this->contactdetails = contactdetails;
+}
+//getting contactdetails
+ContactDetails Country ::getContactDetails()
+{
+    return this->contactdetails;
+}
