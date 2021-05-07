551367f0aba1043e99331f8404a54d01c28045b7
Pooja Patel <patelpooja126018@gmail.com>
2021-05-07 11:30:04 +0530

    comments added

--- Customer.h
+++ Customer.h
@@ -11,18 +11,24 @@ private:
 	std::string pincode;
 	
 public:
+	//default constructor
 	Customer();
+	//parameterized  constructor
 	Customer(std::string name,std::string address,std::string pincode);
-
+	
+	//getter for name
 	std::string getName();
+	//setter for name
 	void setName(std::string name);
-	
+	//getter for address
 	std::string getAddress();
+	//setter for address
 	void setAddress(std::string address);
-	
+	//getter for pincode
 	std::string getPincode();
+	//setter for pincode
 	void setPincode(std::string pincode);
-	
+	//display
 	void display();
 };
 
