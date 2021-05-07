50e4653ff981c24bc3d36eaadaefd517dbea8473
Pooja Patel <patelpooja126018@gmail.com>
2021-05-07 11:34:59 +0530

    Added comments

--- area_details.h
+++ area_details.h
@@ -20,28 +20,32 @@ class area_details{
 	map<int,map<int,string>>::iterator itr;
 	map<int,int>::iterator otr;
 public:
+	
 	static area_details *instance;
 
 	area_details *getInstance();
 	
 	ContactDetails *contactdetails=contactdetails->getInstance();
 
-
+	
 	bool print_all();
+	//adding code
 	bool addCode(int CountryCode, int areaCode, string newAreaName );
-
+	//searching area as per countrycode and area code
 	bool Search(int CountryCode,int areaCode);
-
+	//count total areas
 	int Count();
+	//count total areas country wise
 	int Count(int CountryCode);
-
+	//updating area with countrycode and areacode
 	bool update(int CountryCode,int areaCode);
-
+	//adding country
 	void addCountry(int countrycode);
-
-void deleteCountry(int countrycode);
-
-void updateCountrycode(int oldcountrycode,int newcountrycode);
+	//deleting country
+	void deleteCountry(int countrycode);
+	//updating country
+	void updateCountrycode(int oldcountrycode,int newcountrycode);
+	//deleting area with country and areacode
 	bool Delete(int CountryCode, int areaCode);
 
 };
